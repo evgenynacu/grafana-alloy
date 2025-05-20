6676695287fe0fc02ea3f34c404593fe3a6e2445
Eugene Nacu <evgeny.nacu@gmail.com>
2025-05-20 21:03:00 +0300

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,8 +1,2 @@
 #!/bin/sh
-set -e
-
-# Заменяем переменные окружения в конфиге
-envsubst < /etc/config.alloy.tmpl > /etc/config.alloy.hcl
-
-# Запускаем alloy
 exec /usr/bin/alloy run /etc/config.alloy.hcl
